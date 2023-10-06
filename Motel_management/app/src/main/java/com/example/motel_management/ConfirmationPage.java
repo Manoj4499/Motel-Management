3a93feacf387d3e66d269c3dff5541c73889e916
kallapavankumarreddy <kallapavanreddy23@gmail.com>
2023-10-06 12:01:08 -0500

    COnformation page is created

--- Motel_management/app/src/main/java/com/example/motel_management/ConfirmationPage.java
+++ Motel_management/app/src/main/java/com/example/motel_management/ConfirmationPage.java
@@ -1,23 +1,22 @@
 package com.example.motel_management;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class Billing extends AppCompatActivity {
+public class ConfirmationPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_billing);
-        Button loginButton = findViewById(R.id.ConfirmBT);
+        setContentView(R.layout.activity_confirmation_page);
+        Button loginButton = findViewById(R.id.homeBTN);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Billing.this, ConfirmationPage.class);
+                Intent intent = new Intent(ConfirmationPage.this, SecondActivity.class);
 
                 startActivity(intent);
             }
