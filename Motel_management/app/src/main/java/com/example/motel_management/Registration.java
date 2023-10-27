d38250c4cdbcbb57cb5a0d5c42a57f73d2ea3990
Manoj4499 <s561637@nwmissouri.edu>
2023-10-27 16:57:41 -0500

    allignment changed and in second class added logout button and action

--- Motel_management/app/src/main/java/com/example/motel_management/Registration.java
+++ Motel_management/app/src/main/java/com/example/motel_management/Registration.java
@@ -1,32 +1,26 @@
 package com.example.motel_management;
 
-
-import static android.content.ContentValues.TAG;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-
 public class Registration extends AppCompatActivity {
-
-    TextInputEditText editTextEmail,editTextPassword;
+    TextInputEditText editTextEmail, editTextPassword;
     Button buttonReg;
     FirebaseAuth mAuth;
     ProgressBar progressbar;
@@ -35,10 +29,10 @@ public class Registration extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
+        // Check if the user is already signed in and update the UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Intent intent= new Intent(getApplicationContext(),MainActivity.class);
+        if (currentUser != null) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
             finish();
         }
@@ -48,38 +42,33 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        mAuth=FirebaseAuth.getInstance();
-        editTextEmail=findViewById(R.id.email);
-        editTextPassword=findViewById(R.id.password);
-        buttonReg=findViewById(R.id.btn_register);
+        mAuth = FirebaseAuth.getInstance();
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        buttonReg = findViewById(R.id.btn_register);
         progressbar = findViewById(R.id.progressbar);
-        loginnow=findViewById(R.id.loginnow);
+        loginnow = findViewById(R.id.loginnow);
+
         loginnow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(getApplicationContext(),Login.class);
+                Intent intent = new Intent(getApplicationContext(), Login.class);
                 startActivity(intent);
                 finish();
             }
         });
 
         buttonReg.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
                 progressbar.setVisibility(View.VISIBLE);
-                String email,password;
-                email = String.valueOf(editTextEmail.getText().toString());
-                password = String.valueOf(editTextPassword.getText().toString());
+                String email, password;
+                email = editTextEmail.getText().toString();
+                password = editTextPassword.getText().toString();
 
-
-                if(TextUtils.isEmpty(email)){
-                    Toast.makeText(Registration.this,"Enter email",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                if(TextUtils.isEmpty(password)){
-                    Toast.makeText(Registration.this,"Enter password",Toast.LENGTH_SHORT).show();
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(Registration.this, "Enter both email and password", Toast.LENGTH_SHORT).show();
+                    progressbar.setVisibility(View.GONE);
                     return;
                 }
 
@@ -89,14 +78,14 @@ public class Registration extends AppCompatActivity {
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 progressbar.setVisibility(View.GONE);
                                 if (task.isSuccessful()) {
-
-                                    Toast.makeText(Registration.this, "Account created.",
-                                            Toast.LENGTH_SHORT).show();
+                                    // Registration was successful
+                                    Toast.makeText(Registration.this, "Account created.", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
                                 } else {
-                                    // If sign in fails, display a message to the user.
-                                    Toast.makeText(Registration.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-
+                                    // Registration failed
+                                    Toast.makeText(Registration.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
                                 }
                             }
                         });
