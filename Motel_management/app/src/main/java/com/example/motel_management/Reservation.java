23666cc8499aca6deceb2cc1d4c3f7c82039e5db
Manoj4499 <s561637@nwmissouri.edu>
2023-10-27 18:33:23 -0500

    onClick action for logout and reseravtion and changed to another activity (manoj S561637)

--- Motel_management/app/src/main/java/com/example/motel_management/Reservation.java
+++ Motel_management/app/src/main/java/com/example/motel_management/Reservation.java
@@ -1,14 +1,39 @@
 package com.example.motel_management;
 
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Reservation extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation);
+
+        Button reserveButton = findViewById(R.id.reserveBTN);
+
+        reserveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Reservation.this, Billing.class);
+                startActivity(intent);
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(Reservation.this, Login.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
     }
 }
