7a2de99f49fc8c3b780b2496774e1073ecc9ad57
Manoj4499 <s561637@nwmissouri.edu>
2023-10-27 17:16:18 -0500

    dash borad, logout, is added in both java files(manoj S561637)

--- Motel_management/app/src/main/java/com/example/motel_management/FindReservation.java
+++ Motel_management/app/src/main/java/com/example/motel_management/FindReservation.java
@@ -2,13 +2,43 @@ package com.example.motel_management;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FindReservation extends AppCompatActivity {
+    Button button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_reservation);
+        button = findViewById(R.id.logout);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(FindReservation.this, Login.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        Button dashboardbtn = findViewById(R.id.dashboardBtn);
+        dashboardbtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FindReservation.this, SecondActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
--- Motel_management/app/src/main/java/com/example/motel_management/SecondActivity.java
+++ Motel_management/app/src/main/java/com/example/motel_management/SecondActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SecondActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        button=findViewById(R.id.logout);
         ImageButton makeReservationButton = findViewById(R.id.imageButtonMakeReservation);
         ImageButton findReservationButton = findViewById(R.id.imageButtonFindReservation);
         makeReservationButton.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +31,16 @@ public class SecondActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(SecondActivity.this, FindReservation.class);
                 startActivity(intent);
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent= new Intent(SecondActivity.this,Login.class);
+                startActivity(intent);
+                finish();
+
             }
         });
     }
