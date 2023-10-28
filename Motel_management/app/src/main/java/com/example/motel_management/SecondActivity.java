4ab319abdadc883597336b812f04637747e9d74f
Supreeth <psaisupreeth10111997@gmail.com>
2023-10-27 21:39:55 -0500

    Changes done in second activity (Supreeth S559497)

--- Motel_management/app/src/main/java/com/example/motel_management/SecondActivity.java
+++ Motel_management/app/src/main/java/com/example/motel_management/SecondActivity.java
@@ -22,7 +22,7 @@ public class SecondActivity extends AppCompatActivity {
         makeReservationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SecondActivity.this,Registration.class);
+                Intent intent = new Intent(SecondActivity.this,Reservation.class);
                 startActivity(intent);
             }
         });
