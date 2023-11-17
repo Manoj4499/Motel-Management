128cc96534380f08220fd8e01bb61cce03044777
Manoj4499 <s561637@nwmissouri.edu>
2023-11-17 13:18:12 -0600

    added rooms list adapter

--- Motel_management/app/src/main/java/com/example/motel_management/RoomsListAdapter.java
+++ Motel_management/app/src/main/java/com/example/motel_management/RoomsListAdapter.java
@@ -1,142 +1,37 @@
-package com.example.motel_management;
+package com.example.motel_management;// ExampleAdapter.java
 
-import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.FieldValue;
-import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
-public class ReservationDetailsActivity extends AppCompatActivity {
+public class RoomsListAdapter extends RecyclerView.Adapter<RoomsListAdapter.RoomsViewHolder> {
+    private final List<Room> dataList;
+    private final Context context;
 
-    Room room;
-    TextView tvRoomType, tvDesc;
-    ImageView iv;
-    Button checkInBtn, checkOutBtn, dashboardBtn;
+    public RoomsListAdapter(Context context, List<Room> RoomList) {
+        this.context = context;
+        this.dataList = RoomList;
+    }
 
-    ProgressDialog progressDialog;
-
+    @NonNull
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reservation_details);
-
-        progressDialog = new ProgressDialog(this);
-        tvDesc = findViewById(R.id.tvDesc);
-        tvRoomType = findViewById(R.id.tvType);
-        iv = findViewById(R.id.iv);
-        checkInBtn = findViewById(R.id.btCheckIn);
-        checkOutBtn = findViewById(R.id.btCheckOut);
-        dashboardBtn = findViewById(R.id.btDashboard);
-
-        Bundle bundle = getIntent().getExtras();
-        if (bundle != null) {
-            room = bundle.getParcelable("room");
-        }
-
-        setUI();
-
-        checkInBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                checkIn();
-            }
-        });
-
-        checkOutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                checkOut();
-            }
-        });
-
-        dashboardBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ReservationDetailsActivity.this, MainActivity.class);
-                startActivity(intent);
-                finishAffinity();
-            }
-        });
-
+    public RoomsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_rooms, parent, false);
+        return new RoomsViewHolder(view);
     }
 
-    private void setUI() {
-        if (room != null) {
-            tvDesc.setText(room.getDescription());
-            tvRoomType.setText(room.gettype());
-            Glide.with(this).load(room.getImageUrl()).into(iv);
-            if (room.getCheckIn()) {
-                checkOutBtn.setVisibility(View.VISIBLE);
-            } else {
-                checkInBtn.setVisibility(View.VISIBLE);
-            }
-        }
-    }
-
-    private void checkIn() {
-        progressDialog.show();
-        Map<String, Object> updates = new HashMap<>();
-
-        updates.put("checkInn", true);
-
-        FirebaseFirestore.getInstance().collection("Motels")
-                .document(room.getMotelId()).collection("rooms").document(room.getId())
-                .update(updates).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        progressDialog.hide();
-                        if (task.isSuccessful()) {
-                            Toast.makeText(ReservationDetailsActivity.this, "Check In Success", Toast.LENGTH_SHORT).show();
-                            checkOutBtn.setVisibility(View.VISIBLE);
-                            checkInBtn.setVisibility(View.GONE);
-                        } else {
-                            Toast.makeText(ReservationDetailsActivity.this, "Unable to check in", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
-
-    private void checkOut() {
-        progressDialog.show();
-        Map<String, Object> updates = new HashMap<>();
-
-        updates.put("checkInn", false);
-        updates.put("occupied", false);
-        updates.put("reservationConfirmationNumber", FieldValue.delete());
-        updates.put("reservationFirstName", FieldValue.delete());
-        updates.put("reservationLastName", FieldValue.delete());
-        updates.put("reservation", FieldValue.delete());
-
-
-        FirebaseFirestore.getInstance().collection("Motels")
-                .document(room.getMotelId()).collection("rooms").document(room.getId())
-                .update(updates).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(ReservationDetailsActivity.this, "Check Out Success", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(ReservationDetailsActivity.this, MainActivity.class);
-                            startActivity(intent);
-                            finishAffinity();
-                        } else {
-                            Toast.makeText(ReservationDetailsActivity.this, "Unable to check out", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
 }
