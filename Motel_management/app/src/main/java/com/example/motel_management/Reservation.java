ddbf6df4967be050d3b86238557786744d70ae71
Manoj4499 <s561637@nwmissouri.edu>
2023-11-17 11:00:36 -0600

    toast for id (manoj S561637)

--- Motel_management/app/src/main/java/com/example/motel_management/Reservation.java
+++ Motel_management/app/src/main/java/com/example/motel_management/Reservation.java
@@ -53,11 +53,22 @@ public class Reservation extends AppCompatActivity {
         reserveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 if (TextUtils.isEmpty(String.valueOf(firstNameET.getText()))) {
                     Toast.makeText(Reservation.this, "Enter first name", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                
+
+
+
+
+
+
+
+                if (TextUtils.isEmpty(String.valueOf(idProofET.getText()))) {
+                    Toast.makeText(Reservation.this, "Enter ID Proof number", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
 
 
