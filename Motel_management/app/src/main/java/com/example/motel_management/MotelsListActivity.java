d736751ba05be1561a40237c0b4572fd9778afb4
mkakhilkumar <mkakhilkumar@gmail.com>
2023-11-17 12:28:01 -0600

    onCreate for Activity Motels

--- Motel_management/app/src/main/java/com/example/motel_management/MotelsListActivity.java
+++ Motel_management/app/src/main/java/com/example/motel_management/MotelsListActivity.java
@@ -1,4 +1,53 @@
 package com.example.motel_management;
 
-public class MotelsListActivity {
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MotelsListActivity extends AppCompatActivity {
+
+    ProgressDialog progressDialog;
+    RecyclerView recyclerView;
+    MotelsListAdapter adapter;
+
+    Button btnDashboard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motels);
+
+        progressDialog = new ProgressDialog(this);
+
+        recyclerView = findViewById(R.id.rv);
+        btnDashboard = findViewById(R.id.btnDashboard);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        getData();
+
+
+
+    }
+
+
+    }
 }
