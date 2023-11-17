469fe738f6131c8dd45d06ac34f4fa6f7fae9cbc
Manoj4499 <s561637@nwmissouri.edu>
2023-11-17 11:33:17 -0600

    variables is created ( manoj S561637)

--- Motel_management/app/src/main/java/com/example/motel_management/Room.java
+++ Motel_management/app/src/main/java/com/example/motel_management/Room.java
@@ -1,14 +1,44 @@
 package com.example.motel_management;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-import android.os.Bundle;
+import androidx.annotation.NonNull;
 
-public class Room extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rooms);
+public class Room implements Parcelable {
+    private String id;
+
+    private String motelId;
+    private String type;
+    private String description;
+    private Boolean occupied;
+    private String imageUrl;
+    private Long price;
+
+    private Boolean checkIn;
+
+    public Room(String id, String motelId, String type, String description, Boolean occupied, String imageUrl, Long price, Boolean checkIn) {
+        this.id = id;
+        this.motelId = motelId;
+        this.type = type;
+        this.description = description;
+        this.occupied = occupied;
+        this.imageUrl = imageUrl;
+        this.price = price;
+        this.checkIn = checkIn;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
