ab6cd1879077bf8c43a9eb71ba34ce9273db6408
kallapavankumarreddy <kallapavanreddy23@gmail.com>
2023-11-17 12:48:53 -0600

    added variables in reservation details

--- Motel_management/app/src/main/java/com/example/motel_management/ReseravtionDetailsActivity.java
+++ Motel_management/app/src/main/java/com/example/motel_management/ReseravtionDetailsActivity.java
@@ -1,14 +1,59 @@
 package com.example.motel_management;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-public class ReseravtionDetailsActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReservationDetailsActivity extends AppCompatActivity {
+
+    Room room;
+    TextView tvRoomType, tvDesc;
+    ImageView iv;
+    Button checkInBtn, checkOutBtn, dashboardBtn;
+
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reseravtion_details);
+        setContentView(R.layout.activity_reservation_details);
+
+        progressDialog = new ProgressDialog(this);
+        tvDesc = findViewById(R.id.tvDesc);
+        tvRoomType = findViewById(R.id.tvType);
+        iv = findViewById(R.id.iv);
+        checkInBtn = findViewById(R.id.btCheckIn);
+        checkOutBtn = findViewById(R.id.btCheckOut);
+        dashboardBtn = findViewById(R.id.btDashboard);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            room = bundle.getParcelable("room");
+        }
+
+        setUI();
+
+
+
     }
+
+
+
 }
