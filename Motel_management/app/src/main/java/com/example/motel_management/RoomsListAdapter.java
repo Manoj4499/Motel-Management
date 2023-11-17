a9c0bd6cad0f0582ca7338b173a4cbd170c09cbe
Manoj4499 <s561637@nwmissouri.edu>
2023-11-17 13:19:15 -0600

    rooms available and occupied is added

--- Motel_management/app/src/main/java/com/example/motel_management/RoomsListAdapter.java
+++ Motel_management/app/src/main/java/com/example/motel_management/RoomsListAdapter.java
@@ -34,4 +34,43 @@ public class RoomsListAdapter extends RecyclerView.Adapter<RoomsListAdapter.Room
         return new RoomsViewHolder(view);
     }
 
+    @Override
+    public void onBindViewHolder(RoomsViewHolder holder, int position) {
+        Room currentItem = dataList.get(position);
+        holder.tvDesc.setText(currentItem.getDescription());
+        holder.tvType.setText(currentItem.gettype());
+        holder.tvPrice.setText("$ " + currentItem.getPrice());
+        holder.tvDesc.setText(currentItem.getDescription());
+        String isAvailable;
+        if (currentItem.getOccupied()) {
+            isAvailable = "Room Occupied";
+        } else {
+            isAvailable = "Room Available";
+        }
+        holder.tvOccupied.setText(isAvailable);
+        Glide.with(context).load(currentItem.getImageUrl()).into(holder.iv);
+        if (currentItem.getOccupied()) {
+            holder.btMakeRes.setVisibility(View.GONE);
+        } else {
+            holder.btMakeRes.setVisibility(View.VISIBLE);
+        }
+        holder.btMakeRes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, Reservation.class);
+                Bundle bundle = new Bundle();
+                bundle.putParcelable("room", currentItem);
+                intent.putExtras(bundle);
+                context.startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+
 }
