cea3b927f4b82ff661b7320b3da270d01925bcb2
Supreeth <psaisupreeth10111997@gmail.com>
2023-11-17 12:16:45 -0600

    Made Changes to the find reservation page

--- Motel_management/app/src/main/java/com/example/motel_management/FindReservation.java
+++ Motel_management/app/src/main/java/com/example/motel_management/FindReservation.java
@@ -1,44 +1,77 @@
 package com.example.motel_management;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class FindReservation extends AppCompatActivity {
-    Button button;
-
-
+    Button button, dashboardBtn, btFindReservation;
+    EditText etFirstName, etLastName, etConfirmation;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_reservation);
+
+        progressDialog = new ProgressDialog(this);
+
         button = findViewById(R.id.logout);
+        dashboardBtn = findViewById(R.id.dashboardBtn);
+        btFindReservation = findViewById(R.id.btFindReservation);
+
+        etFirstName = findViewById(R.id.etFirstName);
+        etLastName = findViewById(R.id.etLastName);
+        etConfirmation = findViewById(R.id.etConfirmation);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
                 Intent intent = new Intent(FindReservation.this, Login.class);
                 startActivity(intent);
-                finish();
-
+                finishAffinity();
             }
         });
 
-        Button dashboardbtn = findViewById(R.id.dashboardBtn);
-        dashboardbtn.setOnClickListener(new View.OnClickListener()
-        {
+
+        dashboardBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(FindReservation.this, SecondActivity.class);
-
+                Intent intent = new Intent(FindReservation.this, MainActivity.class);
                 startActivity(intent);
+                finishAffinity();
             }
         });
+
+
+
+  
+
+        progressDialog.show();
+        FirebaseFirestore.getInstance().collectionGroup("rooms")
+                .whereEqualTo("reservationConfirmationNumber", etConfirmation.getText().toString())
+                .whereEqualTo("reservationFirstName", etFirstName.getText().toString().toLowerCase())
+                .whereEqualTo("reservationLastName", etLastName.getText().toString().toLowerCase())
+                .limit(1)
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+
     }
+
 }
